be028bbf7e0e2ee2e5f300e3eda8349265fff74b
KimYunsang-v <qwdbstkd@naver.com>
2020-05-10 18:52:12 +0900

    init project

---
+++ src/main/java/com/hackday/sns_timeline/SnsTimelineApplication.java
@@ -0,0 +1,13 @@
+package com.hackday.sns_timeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsTimelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsTimelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackday/sns_timeline/SnsTimelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackday.sns_timeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsTimelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
