3287514c54bb4277106f74fc83f83ccc3b990713
leejiwoo2002 <55074799+leejiwoo2002@users.noreply.github.com>
2020-05-12 22:16:05 +0900

    Bean 속성 제거

--- src/main/java/com/hackday/sns_timeline/SnsTimelineApplication.java
+++ src/main/java/com/hackday/sns_timeline/SnsTimelineApplication.java
@@ -2,8 +2,6 @@ package com.hackday.sns_timeline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 @SpringBootApplication
 public class SnsTimelineApplication {
