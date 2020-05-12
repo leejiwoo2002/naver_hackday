dc275bf242ffe66022cc5f548116dd90c20880f6
KimYunsang-v <qwdbstkd@naver.com>
2020-05-12 21:49:38 +0900

    sns_timeline_2-15 [윤상&지우] 친구 찾기
    - 친구찾기 테스트 코드 작성

--- src/main/java/com/hackday/sns_timeline/service/MemberSearchService.java
+++ src/main/java/com/hackday/sns_timeline/service/MemberSearchService.java
@@ -16,26 +16,14 @@ import lombok.extern.log4j.Log4j2;
 public class MemberSearchService {
 
 	final private MemberRepository memberRepository;
-	final private SignService signService;
 
 	public Page<Member> findMembers(String search, Pageable pageable) {
+		// pageable 에서는 시작페이지가 0 이라서 0번째 페이지를 찾는게 아니면 1을 빼주고 검색
 		int page = (pageable.getPageNumber() == 0) ? 0 : (pageable.getPageNumber() - 1);
+		// pageable 객체 커스텀
 		pageable = PageRequest.of(page, 10);
+		// 페이지 객체로 찾아옴
 		Page<Member> searchMembers = memberRepository.searchMember(search, pageable);
 		return searchMembers;
-	}
-
-	public void addSubscribe(Long targetId, String email) throws Exception {
-		Member byEmail = memberRepository.findByEmail(email).orElseThrow(() -> new Exception("can't find email"));
-
-		Member target = memberRepository.findById(targetId).orElseThrow(() -> new Exception("can't find email"));
-
-		if (!byEmail.getSubscribe().contains(target)) {
-			log.info("add");
-			byEmail.getSubscribe().add(target);
-			memberRepository.save(byEmail);
-		} else {
-			log.info("already subscribe");
-		}
 	}
 }
