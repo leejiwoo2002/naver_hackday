e74f5c2170c60002f788a5ddbf10745be7f0d673
leejiwoo2002 <leejiwoo2004@kyonggi.ac.kr>
2020-05-20 20:48:50 +0900

    redis 적용 전 백업

--- src/main/java/com/hackday/sns_timeline/SnsTimelineApplication.java
+++ src/main/java/com/hackday/sns_timeline/SnsTimelineApplication.java
@@ -2,6 +2,7 @@ package com.hackday.sns_timeline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 public class SnsTimelineApplication {
--- src/main/java/com/hackday/sns_timeline/content/service/ContentService.java
+++ src/main/java/com/hackday/sns_timeline/content/service/ContentService.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -28,7 +30,7 @@ public class ContentService {
 	final private ContentRepository contentRepository;
 	final private MemberRepository memberRepository;
 
-
+	//@Cacheable
 	public Content contentCreate(ContentDto contentDto, User user,String saveName) throws Exception {
 
 		LocalDateTime currentDateTime = LocalDateTime.now();
@@ -49,7 +51,7 @@ public class ContentService {
 
 		return contentRepository.save(content);
 	}
-
+	//@Cacheable
 	public Page<ContentDto> findMyContent(String userName, Pageable pageable) {
 		// pageable 에서는 시작페이지가 0 이라서 0번째 페이지를 찾는게 아니면 1을 빼주고 검색
 		int page = (pageable.getPageNumber() == 0) ? 0 : (pageable.getPageNumber() - 1);
@@ -63,7 +65,7 @@ public class ContentService {
 		Page<ContentDto> searchMyContent = contentRepository.searchMyContent(member.getId(), pageable).map(ContentDto::customConverter);
 		return searchMyContent;
 	}
-
+	//@Cacheable
 	public Page<ContentDto> getMyTimelineContent(List<MemberDto> memberDtoList, Pageable pageable) {
 		// pageable 에서는 시작페이지가 0 이라서 0번째 페이지를 찾는게 아니면 1을 빼주고 검색
 		int page = (pageable.getPageNumber() == 0) ? 0 : (pageable.getPageNumber() - 1);
@@ -80,12 +82,13 @@ public class ContentService {
 		Page<ContentDto> searchMyContent = contentRepository.searchMyTimelineContent(idList, pageable).map(ContentDto::customConverter);
 		return searchMyContent;
 	}
-
+	//@CacheEvict
 	public void contentRemove(Long id) {
 		contentRepository.removeMyContent(id);
 
 	}
 
+	//@CacheEvict
 	public void contentUpdate(Long id, String title, String body) throws Exception {
 
 		contentRepository.updateMyContent(id,title,body);
